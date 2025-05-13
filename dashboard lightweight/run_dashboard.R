431d31d4ec2f378a5afda9ac96a71341989385a2
abbylewis <aslewis@vt.edu>
2025-05-13 09:25:55 -0400

    dashboard updates

--- dashboard lightweight/run_dashboard.R
+++ dashboard lightweight/run_dashboard.R
@@ -5,6 +5,9 @@ install.packages("plotly")
 install.packages("here")
 library(quarto)
 
+unlink(here::here("dashboard lightweight","data","*"))
+copy_target <- file.copy(here::here("processed_data","L0.csv"), here::here("dashboard lightweight", "data"), overwrite = T)
+
 # Deploy
 quarto_publish_app(input = here::here("dashboard lightweight"), 
                    server = "shinyapps.io")
