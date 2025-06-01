5681e0317a0ccdb048b6fab3348159f52e368e4f
abbylewis <aslewis@vt.edu>
2025-05-31 22:35:35 -0400

    reruns

--- dashboard lightweight/run_dashboard.R
+++ dashboard lightweight/run_dashboard.R
@@ -26,14 +26,14 @@ error_check <- data %>%
             r2_check_co2 = mean(CO2_R2[1:3])) %>%
   filter(r2_check_ch4 < 0.7 & r2_check_co2 < 0.7)
 
-slackr::slackr_setup(
-  channel = "#genx_bot",
-  username = "GENX QAQC bot",
-  token = Sys.getenv("SLACKRTOKEN"),
-  incoming_webhook_url = Sys.getenv("SLACKRURL")
-)
-
 if(nrow(error_check) > 0) {
+  slackr::slackr_setup(
+    channel = "#genx_bot",
+    username = "GENX QAQC bot",
+    token = Sys.getenv("SLACKRTOKEN"),
+    incoming_webhook_url = Sys.getenv("SLACKRURL")
+  )
+  
   slackr::slackr_msg(paste0("Hi team! I noticed that CO2 and CH4 R2 values have been low recently for the following chamber(s):\n",
                    paste(error_check$MIU_VALVE, collapse = ", "),
                    "\nYou might want to take a quick look at the dashboard and make sure things look okay:\n",
